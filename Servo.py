7131bdbef75b9e5e51169366dc900d477f477c2c
Anat Wax <anatwax@gmail.com>
2024-03-12 21:51:05 +0200

    Adding Servo class and servo sub classes to control the animatron movements

---
+++ Servo.py
@@ -0,0 +1,148 @@
+import maestro
+import random
+
+maestro_controller = maestro.Controller(ttyStr='/dev/ttyACM0')
+
+
+class Servo:
+	def __init__(
+		self,
+		max_value=None,
+		min_value=None,
+		pin_number=None,
+	):
+		# pin_number (int): servo number on the Pololu Maestro controller
+		self.max_value = max_value
+		self.min_value = min_value
+		self.pin_number = pin_number
+		self.mid_value = int((min_value + max_value) / 2)
+	
+	def move(self, target_value):
+		maestro_controller.setTarget(self.pin_number,target_value)
+		return
+	
+	def generate_random_value(self, destiny):
+		current_position = maestro_controller.getPosition(self.pin_number)
+		if current_position == destiny:
+			return current_position
+		# Make sure that the higer number is the current_position
+		if current_position < destiny:
+			return random.randint(current_position, destiny)
+		return random.randint(destiny, current_position)
+
+	
+	def move_max(self, target_value=None):
+		if not target_value:
+			target_value = self.generate_random_value(destiny=self.max_value)
+		self.move(target_value=target_value)
+	
+	def move_min(self, target_value=None):
+		if not target_value:
+			target_value = self.generate_random_value(destiny=self.min_value)
+		self.move(target_value=target_value)
+
+
+class Mouth(Servo):
+	def __init__(
+		self,
+		max_value,
+		min_value,
+		pin_number,
+	):
+		super().__init__(
+			max_value,
+			min_value,
+			pin_number,
+		)
+	
+	def open(self, target_value=None):
+		self.move_max(target_value)
+	
+	def close(self, target_value=None):
+		self.move_min(target_value)		
+
+
+class HeadUpDown(Servo):
+	def __init__(
+		self,
+		max_value,
+		min_value,
+		pin_number,
+	):
+		super().__init__(
+			max_value,
+			min_value,
+			pin_number,
+		)
+	
+	def move_up(self, target_value=None):
+		self.move_max(target_value)
+	
+	def move_down(self, target_value=None):
+		self.move_min(target_value)		
+		
+		
+class HeadLeftRight(Servo):
+	# lower numbers will turn left, higher numbers will turn right ->
+	# min_value => left, max_value => right
+	def __init__(
+		self,
+		max_value,
+		min_value,
+		pin_number,
+	):
+		super().__init__(
+			max_value,
+			min_value,
+			pin_number,
+		)
+	
+	def move_left(self, target_value=None):
+		self.move_min(target_value)
+	
+	def move_right(self, target_value=None):
+		self.move_max(target_value)		
+
+
+class Wings(Servo):
+	# The lower the number the higher the wings will reach ->
+	# max_value => wings down, min_value => wings up
+	def __init__(
+		self,
+		max_value,
+		min_value,
+		pin_number,
+	):
+		super().__init__(
+			max_value,
+			min_value,
+			pin_number,
+		)
+		
+	def move_up(self, target_value=None):
+		self.move_max(target_value)
+	
+	def move_down(self, target_value=None):
+		self.move_min(target_value)		
+
+
+class Body(Servo):
+	# The lower the value is, the higher the body will reach ->
+	# max_value => body down, min_value => body up
+	def __init__(
+		self,
+		max_value,
+		min_value,
+		pin_number,
+	):
+		super().__init__(
+			max_value,
+			min_value,
+			pin_number,
+		)
+
+	def move_up(self, target_value=None):
+		self.move_min(target_value)
+	
+	def move_down(self, target_value=None):
+		self.move_max(target_value)		
--- samuel.py
+++ samuel.py
@@ -12,13 +12,18 @@ from sys import exit
 import os
 import json
 import struct
+from Servo import Mouth, HeadUpDown, HeadLeftRight, Wings, Body
 
 
 LED_PIN = 24 # board pin no. 12
 MOTION_PIN = 17 # board pin no. 11
 TOUCH1_PIN = 27 # board pin no. 13
-# ~ test_sound = "/home/anatw/samuel/raven-call-72946.mp3"
 audio_folder_path = "/home/anatw/samuel/raven sounds"
+FAST_BLINK=4 # 6.7
+SLOW_BLINK=33 # 33.3
+# variables to save the current blink values when changing blinking rate is requested:
+SAVED_FAST_BLINK = FAST_BLINK
+SAVED_SLOW_BLINK = SLOW_BLINK
 
 
 # setup:
@@ -29,12 +34,30 @@ GPIO.setup(TOUCH1_PIN,GPIO.IN) # Set the touch sensor pin as 'in'
 # Initiate the servo object. You can find the correct tty by running 'ls /dev/tty*' and 
 # checking for new ttys before and after connecting the Pololu Maestro using USB.
 maestro_controller = maestro.Controller(ttyStr='/dev/ttyACM0')
-# Initiate the pygame module:
 
 
-class sensors():
+class Sensors():
     touch1 = GPIO.input(TOUCH1_PIN)
+    
 
+    
+
+class Movement():
+    mouth = Mouth(pin_number=0, min_value=6200, max_value=9250)
+    # For head up-down movement, lower numbers will turn down, higher numbers will turn up:
+    head_ud = HeadUpDown(pin_number=1, min_value=4450, max_value=8700)
+    head_rl = HeadLeftRight(pin_number=2, min_value=3300, max_value=7100)
+    wings = Wings(pin_number=3, min_value=4600, max_value=5750)
+    body = Body(pin_number=4, min_value=5000, max_value=6200)
+    
+    def initial_position():
+        # setting all the servoes to the initial value so that they alway know their current position value.
+        Movement.mouth.move(target_value=Movement.mouth.min_value)
+        Movement.head_ud.move(target_value=Movement.head_ud.mid_value)
+        Movement.head_rl.move(target_value=Movement.head_rl.mid_value)
+        Movement.wings.move(target_value=Movement.wings.mid_value)
+        Movement.body.move(target_value=Movement.body.mid_value)
+            
 
 def signal_handler(signal, frame):
     # This function is meant to handle exit by the user (cntl+x).
@@ -42,36 +65,29 @@ def signal_handler(signal, frame):
     exit(0)
 
 
-def blink():
-    #time.sleep(random.uniform(6.7, 33.3))
-    while True:
-        blink_time = 0.115
-        time.sleep(random.uniform(1,3))
-        GPIO.output(LED_PIN,GPIO.LOW)
-        time.sleep(blink_time)
-        GPIO.output(LED_PIN,GPIO.HIGH)
-        time.sleep(blink_time)
-        # ~ print("blinking")
-
-    GPIO.cleanup()
-
-
 def head_pat():
-    # TODO: should blink more often when head is patted.
+    # TODO: count time from last head_pat. If greater than X, play a look_at_me sound.
     while True:
         head_pat = "head_pat"
         audio_track_to_play = Samuel.Speak.choose_random_sound_from_category(category=head_pat)
         if GPIO.input(TOUCH1_PIN):
-            maestro_controller.setAccel(1,4)
-            maestro_controller.setTarget(1,5700)
+            # ~ maestro_controller.setAccel(1,4)
+            # ~ Movement.wings.move_down()
+            time.sleep(random.uniform(0,0.8))
+            Movement.body.move_down()
+            Movement.head_ud.move_up()
+            # ~ maestro_controller.setTarget(1,5700)
             print("Mmmmmm this feels nice!")
             Samuel.Speak.speak(audio_track_to_play=audio_track_to_play)
             time.sleep(0.5)
-            maestro_controller.setTarget(1,4000)
+            # ~ maestro_controller.setTarget(1,4000)
+            Movement.body.move_up()
+            Movement.head_ud.move_down()
 
     maestro_controller.close()
     GPIO.cleanup()
     
+
 
 class Samuel:
     def wake_up():
@@ -80,7 +96,38 @@ class Samuel:
 
     def sleep():
         pass
-    
+        
+    class Blink:
+        def change_blinking_time(fast_blink, slow_blink):
+            global FAST_BLINK
+            global SLOW_BLINK
+            global SAVED_FAST_BLINK
+            global SAVED_SLOW_BLINK
+            SAVED_FAST_BLINK = FAST_BLINK
+            SAVED_SLOW_BLINK = SLOW_BLINK
+            FAST_BLINK = fast_blink
+            SLOW_BLINK = slow_blink
+            
+        def restore_blinking_time():
+            global FAST_BLINK
+            global SLOW_BLINK
+            global SAVED_FAST_BLINK
+            global SAVED_SLOW_BLINK
+            FAST_BLINK = SAVED_FAST_BLINK
+            SLOW_BLINK = SAVED_SLOW_BLINK
+            
+        def blink():
+            while True:
+                blink_duration = 0.115
+                time.sleep(random.uniform(FAST_BLINK,SLOW_BLINK)) #time.sleep(random.uniform(6.7, 33.3))
+                GPIO.output(LED_PIN,GPIO.LOW)
+                time.sleep(blink_duration)
+                GPIO.output(LED_PIN,GPIO.HIGH)
+                # time.sleep(blink_duration)
+                # ~ print("blinking")
+
+            GPIO.cleanup()
+        
     class Speak:
         sound_categories = [
             "head_pat",
@@ -93,15 +140,19 @@ class Samuel:
             with open(os.path.join(audio_folder_path, "raven_sound_names.json")) as servo_sound_names_json:
                 servo_sound_names = json.load(servo_sound_names_json)
                 return random.choice(servo_sound_names[category])
-                # ~ audio_track_to_play ="/home/anatw/samuel/raven sounds/head_pat7.mp3"
+                
         def speak(audio_track_to_play):
             """
             Play an mp3 music file.
             
             Args:
-                audio_track_path (strhigher quality): Path to the music track you want to play.
+                audio_track_to_play (str): The name of the music track to be play.
             """
             maestro_controller.setAccel(0,50)
+            Samuel.Blink.change_blinking_time(fast_blink=1, slow_blink=3) # blink faster
+            # Lower body and raise head:
+            # ~ Movement.move(body=True, body_direction="down")
+            Movement.body.move_down()
             # ~ random_category = random.choice(Samuel.Speak.sound_categories)
             # ~ audio_track_to_play = Samuel.Speak.choose_random_sound_from_category(category=random_category)
             with open(os.path.join(audio_folder_path, "servo_ready_sound_rms_dict.json")) as servo_ready_rms_dict_json:
@@ -130,10 +181,12 @@ class Samuel:
                     if index == (num_frames_in_track - 1):
                         print("\n\n\n\n\n\n\n\n")
                     if rms_values_for_servo == 1:
-                        maestro_controller.setTarget(0,9250)
+                        # ~ maestro_controller.setTarget(Movement.Mouth.mouth_servo, Movement.Mouth.mouth_max)
+                        Movement.mouth.open(target_value=Movement.mouth.max_value)
                     else:
                         # ~ print("naa...")
-                        maestro_controller.setTarget(0,6300)
+                        # ~ maestro_controller.setTarget(Movement.Mouth.mouth_servo, Movement.Mouth.mouth_min)
+                        Movement.mouth.close(target_value=Movement.mouth.min_value)
                     pygame.time.wait(int(frame_duration_for_pygame_to_wait * 950)) # sleep for as long as the frame play - ideal to double by 1040
 
                 timer_end = time.time()
@@ -142,16 +195,18 @@ class Samuel:
                 # ~ if time_passed < audio_file_duration:
                     # ~ raise
                 pygame.quit()
+                Samuel.Blink.restore_blinking_time()
             return
 
         
     
 def main():
-    # Assign handler function for the user termination option (cntl +x):
+    # Assign handler function for the user termination option (cntlraven+x):
     signal.signal(signal.SIGINT, signal_handler)
+    Movement.initial_position()
     try:
         threads = []
-        blinking_thread = threading.Thread(target=blink, daemon=True)
+        blinking_thread = threading.Thread(target=Samuel.Blink.blink, daemon=True)
         threads.append(blinking_thread)
         head_pat_thread = threading.Thread(target=head_pat, daemon=True)
         threads.append(head_pat_thread)
