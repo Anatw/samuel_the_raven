59ee4d86f73cae04ce5da93aba6e0a26ebd4c03b
‪Anat Wax <anatwax@gmail.com>
2024-10-27 22:22:10 +0200

    Move to RPI5 and add face recognition and tracking
     - I moved the project from running on Raspberry pi3 to running on
       Raspberry pi 5.
     - I added face detection abilities and face tracking abbilities
       that influences the raven's behaviour. This is the beginning,
       there are additional behaviours I want to add with this new
       ability.
    
    Signed-off-by: ‪Anat Wax <anatwax@gmail.com>

--- global_state.py
+++ global_state.py
@@ -5,3 +5,5 @@ class Events():
 	look_at_me_event = multiprocessing.Event()
 	head_pat_event = multiprocessing.Event()
 	speaking_event = multiprocessing.Event()
+	face_detected_event = multiprocessing.Event()
+	resume_face_tracking_event = multiprocessing.Event()
